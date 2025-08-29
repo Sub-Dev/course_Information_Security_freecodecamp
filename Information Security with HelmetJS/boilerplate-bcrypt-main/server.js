40c2f3c99daa58c4328099a12df361e727bd2b1c
SubDev <bilomarin@gmail.com>
2025-08-29 15:40:50 -0300

    feat: implementar melhorias na segurança da aplicação com ajustes no middleware e novas configurações de proteção

---
+++ Information Security with HelmetJS/boilerplate-bcrypt-main/server.js
@@ -0,0 +1,28 @@
+'use strict';
+const bcrypt = require('bcrypt');
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const cors = require('cors'); // require it here
+
+const app = express();
+
+app.use(cors()); // enable CORS here
+fccTesting(app);
+
+const saltRounds = 12;
+const myPlaintextPassword = 'sUperpassw0rd!';
+const someOtherPlaintextPassword = 'pass123';
+
+//START_ASYNC -do not remove notes, place code between correct pair of notes.
+
+//END_ASYNC
+
+//START_SYNC
+
+//END_SYNC
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Listening on port:', PORT);
+});
